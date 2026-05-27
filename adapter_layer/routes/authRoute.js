bd443374fb13f003f3173225ab2e75048ffbb8e8
Manalo-2A <manalodexter2508@gmail.com>
2026-05-28 00:21:25 +0800

    update

--- adapter_layer/routes/authRoute.js
+++
@@ -1,8 +0,0 @@
-import * as AuthController from '../controllers/authController.js';
-import express from 'express';
-
-const authRoutes = express.Router();
-
-authRoutes.post('/register', AuthController.registerStudent);
-
-export default authRoutes;
